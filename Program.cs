9bf17b3d79ce87ea7c001b8204d426c203328d1c
alvaro-j-perez <aperezg55@miumg.edu.gt>
2020-11-23 21:52:44 -0600

    radio utilizando c#

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace capsulas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           const double PI = 3.1416;
+           Console.WriteLine("INTRODUCE EL NUMERO DEL RADIO QUE DESEAS: ");
+
+           double radio = double.Parse(Console.ReadLine());
+
+           double area = Math.Pow(radio, 2) * PI;
+
+           Console.WriteLine($"EL AREA DEL CIRCULO ES: {area}");
+           
+        }
+    }
+}
